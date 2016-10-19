1be1ed79e2267f31a87e31d10daf5e0a66849174
Arthur van Kruining <Arthur van Kruining>
2016-10-19 15:44:29 +0200

    3.6 Table model relationships

--- app/models/Project.php
+++ app/models/Project.php
@@ -2,5 +2,8 @@
 
 class Project extends BaseModel
 {
-	
+	public function initialize()
+	{
+		$this->belongsTo('user_id', 'User', 'id');	
+	}
 }
--- app/models/User.php
+++ app/models/User.php
@@ -5,7 +5,10 @@ use \Phalcon\Mvc\Model\Behavior\SoftDelete;
 class User extends BaseModel
 {
 	public function initialize()
-	{
+	{	
+		// local field, referenced model, referenced field.
+		$this->hasMany('id', 'Project', 'user_id');
+
 		$this->addBehavior(new SoftDelete([
 			'field' => 'deleted',
 			'value' => 1
