c4ec668beab998fa4ea2e5dbd1352fe39f45f0f5
Arthur van Kruining <Arthur van Kruining>
2016-10-20 13:06:47 +0200

    5.2 Enable Volt

--- public/index.php
+++ public/index.php
@@ -29,6 +29,9 @@ try {
 		$di->set('view', function() {
 			$view = new \Phalcon\Mvc\View();
 			$view->setViewsDir('../app/views/');
+			$view->registerEngines([
+				".volt" => 'Phalcon\Mvc\View\Engine\Volt'
+				]);
 			return $view;
 		});
 
